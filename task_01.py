c731971b6dfc976a4148aed464e8ebffb9f86152
paisely65 <cunysps@gmail.com>
2016-05-05 15:16:52 -0400

    Tasks copleted.

--- task_01.py
+++ task_01.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module  reates a new Fibonacci Generator."""
+"""This module reates a new Fibonacci Generator."""
 
 
 def xfibo(count):
     """This functions iterates the value of count.
+
     Agrs:
         count (Integer)
+
     Returns:
         yield each number in a Fibonacci sequence starting with 0.
+
     Examples:
-    >>> for num in xfibo(5):
-    print num
-    0
-    1
-    2
-    3
-    4
-    >>> for num in xfibo(2):
-    print num
-    0
-    1
-    >>>for num in xfibo(3):
-    print num
-    0
-    1
-    2
+    >>>
     """
     iteration = 0
     lastnum, curnum = 0, 1
-    numbers = [lastnum]
+
     while iteration < count:
-        numbers.append(curnum)
+        yield lastnum
+        iterations += 1
         lastnum, curnum = curnum, lastnum + curnum
-    return numbers
+
--- task_02.py
+++ task_02.py
@@ -11,13 +11,5 @@ def fibo(count):
             count(integer)
         Returns:
             list comprehension
-        Examples:'
-        >>> fibo(10)
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        >>> fibo (15)
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-        >>> fibo(5)
-        [0, 1, 2, 3, 4]
     """
-    my_comp = [num for num in task_01.xfibo(count)]
-    print my_comp
+    return [num for num in task_01.xfibo(count)]
